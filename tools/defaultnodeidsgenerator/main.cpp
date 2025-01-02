1ad62469a93119956fd6677f133c0f364dd3baa6
Tarja Sundqvist <tarja.sundqvist@qt.io>
2025-01-02 22:10:03 +0200

    Revert "Update commercial SPDX-License-Identifier"
    This reverts commit 1318e10861f134668451b7ea497ccc7c568f3103.
    
    Revert of commercial SPDX license identifiers is required for the
    Qt 6.5.x opensource releases, Qt 6.5.4 onwards.
    
    Change-Id: I56fd79072e3dc609367e151ae1145c1477db4849
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>

--- tools/defaultnodeidsgenerator/main.cpp
+++ tools/defaultnodeidsgenerator/main.cpp
@@ -1,5 +1,5 @@
 // Copyright (C) 2018 basysKom GmbH, opensource@basyskom.com
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 #include <QCommandLineParser>
 #include <QCoreApplication>
@@ -26,7 +26,7 @@ void createLicenseHeader(QTextStream &output)
 {
     output << R"(
 // Copyright (C) 2021 basysKom GmbH, opensource@basyskom.com
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 )";
 }
 
